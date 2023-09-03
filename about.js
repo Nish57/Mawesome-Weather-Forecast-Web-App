01ea35c24b80c0c934ffdddc0f9c1ec2cf0d2f67
Nishtha Tiwari <105223628+Nish57@users.noreply.github.com>
2023-09-03 20:54:58 +0530

    Add files via upload

---
+++ about.js
@@ -0,0 +1,15 @@
+function twt() {
+    window.open('https://t.me/Nishtha2408')
+}
+
+function tele() {
+    window.open('https://twitter.com/pktnishtha24')
+}
+
+function github() {
+    window.open('https://github.com/Nish57')
+}
+
+function linkd() {
+    window.open('https://www.linkedin.com/in/nishtha24/')
+}
